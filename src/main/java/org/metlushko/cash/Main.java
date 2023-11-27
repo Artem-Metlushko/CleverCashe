078dde8d1c82dd37d146ea346424f9ad5c7640cc
“Artem <3947710@gmail.com>
2023-11-27 19:35:32 +0300

    add:feature/2 add pdfprint

--- src/main/java/org/metlushko/cash/Main.java
+++ src/main/java/org/metlushko/cash/Main.java
@@ -4,9 +4,12 @@ import org.metlushko.cash.dao.impl.UserDao;
 import org.metlushko.cash.dto.UserDto;
 import org.metlushko.cash.entity.User;
 import org.metlushko.cash.mapper.UserMapperImpl;
+import org.metlushko.cash.pdf.PdfPrint;
 import org.metlushko.cash.service.UserService;
 import org.metlushko.cash.util.DatabaseInitializerUtil;
 import org.metlushko.cash.util.UuidWrapper;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,7 +26,8 @@ public class Main {
         userService.update(new UserDto("ADAM", "ADAM", "ADAM", "qweqwe@mail.ru", "ADAM"), saveUser);
         userService.delete(saveUser.getUserId());
 
-
+        List<User> all = userService.findAll();
+        PdfPrint.createReport(all);
 
 
     }
